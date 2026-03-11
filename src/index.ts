117d9c2ebebda1996392e44f674e615f10970d80
DPC1012 <devarshpc@gmail.com>
2026-03-11 19:51:08 +0530

    completed the assignment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import authRouter from "./routes/authRoute";
 import serviceRouter from "./routes/serviceroute";
 import appointmentRouter from "./routes/appointmentRoute";
+import providerRouter from "./routes/providerRouter";
 
 const app = express();
 const route = express.Router()
@@ -13,6 +14,7 @@ app.get("/",(req,res) => {
 app.use("/auth", authRouter)
 app.use("/services", serviceRouter)
 app.use("/appointments", appointmentRouter)
+app.use("/providers", providerRouter)
 
 app.listen(3000,() => {
     console.log("backend is running")
